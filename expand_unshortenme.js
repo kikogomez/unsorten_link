6cb3dcb505928eba25e0b3599413e19dca853a73
kikogomez <kikogomez@gmail.com>
2024-07-03 09:17:43 +0100

    Update expand_unshortenme.js

--- expand_unshortenme.js
+++ expand_unshortenme.js
@@ -1,11 +1,13 @@
 function expandWithUnshortenMe() {
     const shortUrl = document.getElementById('shortUrl').value;
     const resultDiv = document.getElementById('result');
+    const rawResponseTextarea = document.getElementById('rawResponse');
     const apiUrl = `https://unshorten.me/json/${shortUrl}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            rawResponseTextarea.value = JSON.stringify(data, null, 2);
             if (data.success && data.resolved_url) {
                 resultDiv.innerHTML = `<strong>URL Original:</strong> <a href="${data.resolved_url}" target="_blank">${data.resolved_url}</a>`;
             } else {
@@ -13,6 +15,8 @@ function expandWithUnshortenMe() {
             }
         })
         .catch(error => {
+            rawResponseTextarea.value = error;
             resultDiv.innerHTML = `<strong>Error:</strong> Ocurrió un problema al intentar expandir la URL.`;
         });
 }
+
