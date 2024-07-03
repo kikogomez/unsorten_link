6e52bed29689fd39977e75f9bb16022b0470130d
Francisco Gomez-Caldito Viseas <kikogomez@gmail.com>
2024-07-03 12:16:59 +0100

    Using a dropdown

--- expand.js
+++ expand.js
@@ -7,19 +7,24 @@ function expandURL() {
 
     switch(service) {
         case 'unshortenme':
-            apiUrl = `https://unshorten.me/json/${shortUrl}`;
+            apiUrl = `https://unshorten.me/json/${encodeURIComponent(shortUrl)}`;
+            expandField = 'resolved_url';
             break;
-        case 'unshortenlink':
+ /*       case 'unshortenlink':
             apiUrl = `https://api.unshorten.link/?url=${encodeURIComponent(shortUrl)}`;
+            expandField = 'unshortened_url';
             break;
         case 'checkshorturl':
             apiUrl = `https://checkshorturl.com/expand.php?url=${encodeURIComponent(shortUrl)}`;
+            expandField = 'long_url';
             break;
         case 'longurl':
             apiUrl = `http://api.longurl.org/v2/expand?url=${encodeURIComponent(shortUrl)}`;
+            expandField = 'long-url';
             break;
         case 'expandurl':
             apiUrl = `https://expandurl.com/api/v1/expand?url=${encodeURIComponent(shortUrl)}`;
+            expandField = 'destination_url';
             break;
         case 'bitly':
             apiUrl = 'https://api-ssl.bitly.com/v4/expand';
@@ -44,7 +49,7 @@ function expandURL() {
                 rawResponseTextarea.value = error.toString();
                 resultDiv.innerHTML = `<strong>Error:</strong> Ocurrió un problema al intentar expandir la URL.`;
             });
-            return; // Exit the function to avoid calling fetch again.
+            return; // Exit the function to avoid calling fetch again. */
         default:
             resultDiv.innerHTML = `<strong>Error:</strong> Servicio no soportado.`;
             return;
@@ -54,8 +59,8 @@ function expandURL() {
         .then(response => response.json())
         .then(data => {
             rawResponseTextarea.value = JSON.stringify(data, null, 2);
-            if (data.unshortened_url || data.long_url) {
-                const originalUrl = data.unshortened_url || data.long_url;
+            if (data[expandField]) {
+                const originalUrl = data[expandField];
                 resultDiv.innerHTML = `<strong>URL Original:</strong> <a href="${originalUrl}" target="_blank">${originalUrl}</a>`;
             } else {
                 resultDiv.innerHTML = `<strong>Error:</strong> No se pudo expandir la URL. Verifica que la URL acortada sea correcta.`;
